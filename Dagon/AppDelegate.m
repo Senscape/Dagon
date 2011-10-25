bbd866305c136eed6d906609c30057453dfdb736
Agustin Cordes <agustin@senscape.net>
2011-10-25 17:09:44 -0300

    Initial Commit

---
+++ Dagon/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  Dagon
+//
+//  Created by Agustín Cordes on 10/25/11.
+//  Copyright (c) 2011 Senscape s.r.l. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ Dagon/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Dagon
+//
+//  Created by Agustín Cordes on 10/25/11.
+//  Copyright (c) 2011 Senscape s.r.l. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
