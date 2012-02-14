6aca1ab7d699f919fc9f7a5f69bf27384ed62e86
Agustín Cordes <agustin@senscape.net>
2012-02-14 00:09:27 -0300

    Pending fixes.

--- Dagon/DGNodeProxy.h
+++ Dagon/DGNodeProxy.h
@@ -90,7 +90,18 @@ public:
         
         lua_pushnil(L);
         while (lua_next(L, 1) != 0) {
-            int dir = lua_tonumber(L, -2);
+            const char* key = lua_tostring(L, -2);
+            int dir;
+            
+            // Ugly nesting of strcmps() but there's probably no better way of doing this...
+			if (strcmp(key, "N") == 0) dir = DGNorth;
+			else if (strcmp(key, "W") == 0) dir = DGWest;
+			else if (strcmp(key, "S") == 0) dir = DGSouth;
+			else if (strcmp(key, "E") == 0) dir = DGEast;
+			else if (strcmp(key, "NW") == 0) dir = DGNorthWest;
+			else if (strcmp(key, "SW") == 0) dir = DGSouthWest;
+			else if (strcmp(key, "SE") == 0) dir = DGSouthEast;
+			else if (strcmp(key, "NE") == 0) dir = DGNorthEast;
             
             if (lua_isfunction(L, -1)) {
                 int ref = luaL_ref(L, LUA_REGISTRYINDEX);
--- Sample/Asylum.lua
+++ Sample/Asylum.lua
@@ -1,5 +1,5 @@
 room 'Cafeteria'
 
 spot = cafeteria1:addSpot( Spot(1, {10, 20, 30, 40}) )
-spot:attach(NE, function() end)
+spot:attach(SW, function() end)
 switch(Cafeteria)
