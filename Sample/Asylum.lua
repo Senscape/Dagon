f722a5fd9d3fa0e5693d41ebe6003f9f61fe9526
Agustín Cordes <agustin@senscape.net>
2012-02-15 20:37:02 -0300

    Implemented the "self" global in callbacks. Improvements to the Luna template.

--- Sample/Asylum.lua
+++ Sample/Asylum.lua
@@ -1,7 +1,6 @@
 room 'Cafeteria'
 
 spot = cafeteria1:addSpot( Spot(EAST, {512, 512, 1536, 512, 1536, 2048, 512, 2048}) )
-spot2 = cafeteria1:addSpot( Spot(WEST, {512, 512, 1536, 512, 1536, 2048, 512, 2048}) )
 spot:attach(CUSTOM, function() self:toggle() end)
-spot2:attach(CUSTOM, function() self:toggle() end)
+
 switch(Cafeteria)
