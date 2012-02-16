237070fcb80a746228292885473455efdd82c5bb
Agustín Cordes <agustin@senscape.net>
2012-02-16 16:33:33 -0300

    Modification to the sample.

--- Sample/Asylum.lua
+++ Sample/Asylum.lua
@@ -1,7 +1,3 @@
 room 'Cafeteria'
 
-spot = cafeteria1:addSpot( Spot(EAST, {512, 512, 1536, 512, 1536, 2048, 512, 2048}) )
-spot:attach(IMAGE, "cafeteria1.tex", 0)
-spot:attach(CUSTOM, function() self:toggle() end)
-
 switch(Cafeteria)
