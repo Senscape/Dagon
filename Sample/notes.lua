675ae7464266501b21958a4f3bcfecda4182aa3d
Agustín Cordes <agustin@senscape.net>
2012-07-01 12:17:59 -0300

    Updated sample to reflect changes in the feedback manager.

--- Sample/notes.lua
+++ Sample/notes.lua
@@ -10,10 +10,15 @@ journal = Journal.create()
 
 journal:addEntry(Notes.TUNNEL, 
 	function()
-		feed("Scary place.")
+		feed("A wise man is said to live there that may be able provide the answers she seeks.", "dlg_00001.ogg")
 	end)
 	
 journal:addEntry(Notes.HOSPITAL,
 	function()
-		feed("I must find the hospital.")
+		feed("Myosotis, the Trader of Stories, has travelled far across the land in search...", "dlg_00002.ogg")
+	end)
+
+journal:addEntry("Test",
+	function()
+		feed("For some time now, she has been told tales of a place where the Great Oak stands and that rocks float in the air.", "dlg_00002.ogg")
 	end)
