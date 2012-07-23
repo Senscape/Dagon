2cee6634099f15dd25155be1e75daebcbb050dfb
Agustín Cordes <agustin@senscape.net>
2012-07-23 15:36:57 -0300

    Incremented build number.

--- Dagon/DGVersion.h
+++ Dagon/DGVersion.h
@@ -16,7 +16,7 @@
 #define DGVersionMajor 1
 #define DGVersionMinor 0
 #define DGVersionRelease 0
-#define DGVersionBuild 1009
+#define DGVersionBuild 1010
 #define DGVersionType "a"
 
 #endif // DG_VERSION_H
