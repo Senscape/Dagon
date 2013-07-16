9cac483cc6df8af14aba150523025b23391d07df
Agustín Cordes <agustin@senscape.net>
2013-07-16 15:47:28 -0300

    Changed license to MPL 2.0.

--- Source/DGFontManager.cpp
+++ Source/DGFontManager.cpp
@@ -1,12 +1,13 @@
 ////////////////////////////////////////////////////////////
 //
 // DAGON - An Adventure Game Engine
-// Copyright (c) 2012 Senscape s.r.l.
+// Copyright (c) 2011-2013 Senscape s.r.l.
 // All rights reserved.
 //
-// NOTICE: Senscape permits you to use, modify, and
-// distribute this file in accordance with the terms of the
-// license agreement accompanying it.
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL was
+// not distributed with this file, You can obtain one at
+// http://mozilla.org/MPL/2.0/.
 //
 ////////////////////////////////////////////////////////////
 
