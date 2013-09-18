84c1b700afe85eee636a1dff8d5a6a35a15d3a90
Agustín Cordes <agustin@senscape.net>
2013-09-18 09:38:15 -0300

    Temporary fix for snprintf on Windows.

--- Source/Defines.h
+++ Source/Defines.h
@@ -91,4 +91,35 @@ enum Directions {
   kCurrent = 0xFF // Maintain current direction
 };
 
+// Temporary fix for Visual Studio
+
+#ifdef _MSC_VER
+
+#define snprintf c99_snprintf
+
+inline int c99_snprintf(char* str, size_t size, const char* format, ...) {
+  int count;
+  va_list ap;
+  
+  va_start(ap, format);
+  count = c99_vsnprintf(str, size, format, ap);
+  va_end(ap);
+  
+  return count;
+}
+
+inline int c99_vsnprintf(char* str, size_t size, const char* format,
+                         va_list ap) {
+  int count = -1;
+  
+  if (size != 0)
+    count = _vsnprintf_s(str, size, _TRUNCATE, format, ap);
+  if (count == -1)
+    count = _vscprintf(format, ap);
+  
+  return count;
+}
+
+#endif // _MSC_VER
+
 #endif // DAGON_DEFINES_H_
