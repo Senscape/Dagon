62b25dea6064b3138255ca3018edcd60d6e129a8
AgustinCordes <agustin@senscape.net>
2013-09-25 15:56:41 -0300

    Updated documentation and build configuration.

--- Readme.md
+++ Readme.md
@@ -1,28 +1,49 @@
-## DAGON - An Adventure Game Engine
+# DAGON - An Adventure Game Engine
 
 ### Description
 
-Dagon is a game engine especially tailored for adventure games and based on open technologies. Featuring a deceptively simple scripting language, blazing fast performance and high portability supporting Windows, Linux, and Mac OS X, the project aims to become a referendum of the genre.
+Dagon is a game engine developed by Senscape especially tailored for adventure
+games and based on open technologies. Featuring a simple scripting language,
+blazing fast performance and high portability supporting Windows, Linux, Mac
+OS X and iOS devices, the project aims to become a reference of the genre. Dagon
+is completely free to use.
+
+_**Please note:** The engine is still under heavy development and script
+compatibility breaking changes may be introduced until version 1.0 is released._
 
 ### Installation
 
-*This is the experimental branch undergoing heavy changes and it may not build on your platform.*
+Dagon can be built using Premake, a straightforward configuration system:
+http://industriousone.com/premake
+
+For more details on how to build on your specific system, please refer to
+premake4.lua.
 
 ### Running
 
-We recommend using the readily available _Asylum Interactive Teaser_ for testing, which also serves as an advanced and highly in-depth tutorial. It can be downloaded from Senscape's website: http://www.senscape.net/asylum-teaser
+We always recommend downloading the official Dagon binaries for deploying your
+games: https://github.com/Senscape/Dagon/releases
 
-Its source code is also available in a GitHub repo: https://github.com/Senscape/Asylum-Teaser
+You may use the readily available _Asylum Interactive Teaser_ for testing, which also serves as an advanced and highly in-depth tutorial. It can be downloaded
+from Senscape's website: http://www.senscape.net/asylum-teaser
 
-To create your own project, copy the Dagon binary in a folder with a _main.lua_ file or indicate the name of your main script in the _Dagon.cfg_ file (included in the Sample folder).
+Its source code is also available in the following GitHub repo:
+https://github.com/Senscape/Asylum-Teaser
+
+To create your own project, copy the Dagon binary in a folder with a _main.lua_
+file or indicate the name of your main script in the Dagon.cfg file included
+in the _Sample_ folder.
 
 ### Support
 
-Feel free to visit our forums to discuss the engine and request technical support: http://www.senscape.net/forum
+Feel free to visit our forums to discuss the engine and request technical
+support: http://www.senscape.net/forum
 
 ### Copyright
 
 Copyright (c) 2011-2013 Senscape s.r.l.
 All rights reserved.
 
-This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+This Source Code Form is subject to the terms of the Mozilla Public License, v.
+2.0. If a copy of the MPL was not distributed with this file, You can obtain one
+at http://mozilla.org/MPL/2.0/.
--- premake4.lua
+++ premake4.lua
@@ -1,7 +1,69 @@
--- Dagon - An Adventure Game Engine
+--[[
+
+DAGON
+An Adventure Game Engine
+
+This is a fairly basic Premake configuration that generates project files for
+your preferred build system. Premake may be downloaded from the following
+site:
+
+http://industriousone.com/premake
+
+Usage is as simple as typing 'premake4 [action]'. Please, do note that this
+configuration targets the native architecture of the host computer and will
+attempt to link dynamically. Also, the Windows configuration only builds as
+a console.
+
+Because this Premake file is primarily intended for building from source and
+contributing to Dagon, we strongly suggest that you download the official binary
+releases for deploying your games:
+
+https://github.com/Senscape/Dagon/releases
+
+Copyright (c) 2011-2013 Senscape s.r.l. All rights reserved.
+
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain
+one at http://mozilla.org/MPL/2.0/.
+
+--]]
+
+--[[
+
+BUILD INSTRUCTIONS
+
+Dagon requires the following dependencies: FreeType, GLEW, Lua 5.1, Ogg, OpenAL,
+Theora, Vorbis, SDL2.
+
+Linux:
+
+  We suggest installing the following packages via apt-get: libfreetype6-dev,
+  libglew-dev, liblua5.1-0-dev, libogg-dev, libopenal-dev, libvorbis-dev,
+  libtheora-dev.
+
+  SDL2 was not available in default repos as of this writing and had to be built
+  from scratch.
+
+Mac OS X:
+
+  We strongly encourage using Homebrew to install packages: http://brew.sh
+
+  Suggested Homebrew formulas are: freetype, glew, lua, libogg, libvorbis,
+  theora, sdl2.
+
+Windows:
+
+  We provide the following binaries packages for your convenience:
+
+  http://www.senscape.net/files/dagon-0.6.0-libs-win-x86.zip
+  http://www.senscape.net/files/dagon-0.6.0-libs-win-x64.zip
+
+]]--
+
+-- Base solution
 solution "Dagon"
   configurations { "Release", "Debug" }
-  platforms { "native", "universal" }
+  platforms { "native" } -- Default to native architecture
   location "Build"
   
   configuration { "Release" }
@@ -14,62 +76,95 @@ solution "Dagon"
   flags { "Symbols" }
   targetdir "Build/Debug"
 
+  -- Clean up if required and exit
   if _ACTION == "clean" then
     os.rmdir("Build")
+    os.exit()
   end
  
+  -- The main Dagon project
   project "Dagon"
     targetname "dagon"
+    -- GLEW_STATIC only applies to Windows, but there's no harm done if defined
+    -- on other systems.
     defines { "GLEW_STATIC", "OV_EXCLUDE_STATIC_CALLBACKS" }
     location "Build"
     objdir "Build/Objects"
      
+    -- Note that we always build as a console app, even on Windows.
     kind "ConsoleApp"
     language "C++"
     files { "Source/**.h", "Source/**.c", "Source/**.cpp" }
     
-    libs_unix = { "freetype", "GL", "GLU", "GLEW", "lua5.1", "openal",
-                  "ogg", "SDL2", "vorbis", "vorbisfile", "theoradec" }
-              
-    if os.get() == "linux" then
-      for i = 1, #libs_unix do
-        local lib = libs_unix[i]
-        if os.findlib(lib) == nil then
-          print ("Library not found:", lib)
-          print "Aborting..."
-          os.exit()
-        end
+    libraries = {}
+    
+    -- Libraries required for Unix-based systems
+    libs_unix = { "freetype", "GLEW", "ogg", "SDL2", 
+                  "vorbis", "vorbisfile", "theoradec" }
+    
+    -- Libraries required for Windows, preferring static binaries
+    libs_win = { "freetype", "glew32s", "libogg_static", 
+                 "libtheora_static", "libvorbis_static", 
+                 "libvorbisfile_static", "lua", "OpenAL32",
+                 "SDL2", "SDL2main", "opengl32", "glu32" }
+
+    -- Attempt to look for Lua library with most commonly used names
+    local lua_lib_names = { "lua-5.1", "lua5.1", "lua" }
+    local lua_lib = { name = nil, dir = nil }
+    for i = 1, #lua_lib_names do
+      lua_lib.name = lua_lib_names[i]
+      lua_lib.dir = os.findlib(lua_lib.name)
+      if(lua_lib.dir ~= nil) then
+        break
+      end
+    end
+  
+    -- Build the libraries table according to the host system
+    if os.is("linux") then
+      libraries = libs_unix
+      table.insert(libraries, "GL")
+      table.insert(libraries, "GLU")
+      table.insert(libraries, "openal")
+      table.insert(libraries, lua_lib.name)
+    elseif os.is("macosx") then
+      libraries = libs_unix
+      table.insert(libraries, lua_lib.name)
+    elseif os.is("windows") then
+      libraries = libs_win
+      table.insert(libraries, lua_lib.name)
+    else
+      print "Attempting to build on unsupported system."
+      print "Aborting..."
+      os.exit()
+    end
+    
+    -- Confirm that all the required libraries are present
+    for i = 1, #libraries do
+      local lib = libraries[i]
+      if os.findlib(lib) == nil then
+        print ("Library not found:", lib)
+        print "Aborting..."
+        os.exit()
       end
     end
 
+    -- Final configuration, includes and links according to the host system
     configuration "linux"
-      includedirs { "/usr/include", "/usr/include/lua5.1", 
+      includedirs { "/usr/include", "/usr/include/lua5.1",
                     "/usr/include/freetype2", "/usr/local/include",
-                    "/usr/local/include/lua5.1", 
+                    "/usr/local/include/lua5.1",
                     "/usr/local/include/freetype2" }
       libdirs { "/usr/lib", "/usr/local/lib" }
-      links { libs_unix }
+      links { libraries }
   
     configuration "macosx"
-      includedirs { "Mac/Headers", "Mac/Headers/freetype2" }
-      libdirs { "Mac/Libraries" }
-      links { "AudioToolbox.framework", "AudioUnit.framework", 
-              "Carbon.framework", "Cocoa.framework", 
-              "CoreAudio.framework", "CoreFoundation.framework",
-              "ForceFeedback.framework", "IOKit.framework",
-              "OpenAL.framework", "OpenGL.framework" }
-      links { "freetype", "GLEW", "lua", "ogg", "SDL2",
-              "vorbis", "vorbisfile", "theoradec" }
+      includedirs { "/usr/include", "/usr/include/lua5.1", 
+                    "/usr/include/freetype2", "/usr/local/include",
+                    "/usr/local/include/lua5.1",
+                    "/usr/local/include/freetype2" }
+      libdirs { "/usr/lib", "/usr/local/lib" }
+      links { "OpenAL.framework", "OpenGL.framework" }
+      links { libraries }
               
     configuration "windows"
-      includedirs { "Windows/Headers", "Windows/Headers/freetype" }
-      configuration "x32"
-        libdirs { "Windows/Libraries/x86" }
-      configuration "x64"
-        libdirs { "Windows/Libraries/x64" }
-      links { "freetype", "glew32s", "libogg_static", 
-              "libtheora_static", "libvorbis_static", 
-              "libvorbisfile_static", "lua", "OpenAL32",
-              "SDL2", "SDL2main", "opengl32", "glu32",
-              "Imm32", "version", "winmm" }
- 
+      links { libraries }
