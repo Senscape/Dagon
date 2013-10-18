c68d36fa84074c9f7fe000cb59c4a22e8215578f
AgustinCordes <agustin@senscape.net>
2013-10-18 17:44:36 -0300

    Incremented version number.

--- src/Version.h
+++ src/Version.h
@@ -16,9 +16,9 @@
 
 #define DAGON_VERSION_MAJOR 0
 #define DAGON_VERSION_MINOR 6
-#define DAGON_VERSION_RELEASE 5
+#define DAGON_VERSION_RELEASE 6
 #define DAGON_VERSION_TYPE "a"
-#define DAGON_BUILD 1007
+#define DAGON_BUILD 1008
 
 #define mkstr(a) # a
 #define mkver(a, b, c) mkstr(a) "." mkstr(b) "." mkstr(c)
