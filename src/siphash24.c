4a26780d1476eb03666301a705e1faaab7cae1a2
bd339 <bd339@users.noreply.github.com>
2016-07-17 12:16:49 +0200

    Update siphash24.c
    This change didn't get picked up from my local repo for some reason. Basically, we don't want to be flooded with debug values in our debug builds.

--- src/siphash24.c
+++ src/siphash24.c
@@ -58,7 +58,7 @@
     v2 = ROTL(v2, 32);                                                         \
   } while (0)
 
-#ifdef DEBUG
+/*#ifdef DEBUG
 #define TRACE                                                                  \
   do {                                                                         \
     printf("(%3d) v0 %08x %08x\n", (int)inlen, (uint32_t)(v0 >> 32),           \
@@ -72,7 +72,8 @@
   } while (0)
 #else
 #define TRACE
-#endif
+#endif*/
+#define TRACE
 
 int siphash(uint8_t *out, const uint8_t *in, uint64_t inlen, const uint8_t *k) {
   /* "somepseudorandomlygeneratedbytes" */
